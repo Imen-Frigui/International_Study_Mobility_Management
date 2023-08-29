3922f2bb349a1b4318f5a2e38e72bce9728d0526
Imen Frigui <imen.frigui@esprit.tn>
2023-08-29 18:09:58 +0100

    v4

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -7,19 +7,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramSubmissionRepository;
 use App\Repository\NotificationRepository;
+use App\Entity\Notification;
+use App\Entity\Student;
 
 class StudentController extends AbstractController
 {
 
     #[Route('/student/submissions', name: 'student_submissions')]
-    public function viewSubmissions(ProgramSubmissionRepository $programSubmissionRepository): Response
+    public function viewSubmissions(ProgramSubmissionRepository $programSubmissionRepository, NotificationRepository $notificationRepository): Response
     {
         // Fetch submissions for student with ID 2
-        $studentId = 2;
+        $studentId = 1;
         $submissions = $programSubmissionRepository->findBy(['student' => $studentId]);
 
+        $student = $this->getDoctrine()->getRepository(Student::class)->find(1);
+        $notifications = $notificationRepository->findBy(['student' => $student, 'hasRead' => false]);
+        
         return $this->render('student/view_submissions.html.twig', [
             'submissions' => $submissions,
+            'notifications' => $notifications,
         ]);
     }
 
@@ -38,4 +44,28 @@ class StudentController extends AbstractController
             'notifications' => $notifications,
         ]);
     }
+
+
+    #[Route('/view-notification/{id}', name: 'view_notification')]
+    public function viewNotification(int $id, NotificationRepository $notificationRepository): Response
+    {
+        $notification = $notificationRepository->find($id);
+
+        if (!$notification) {
+            throw $this->createNotFoundException('Notification not found');
+        }
+
+        $student = $this->getDoctrine()->getRepository(Student::class)->find(1);
+        $notifications = $notificationRepository->findBy(['student' => $student, 'hasRead' => false]);
+
+        // Mark the notification as read
+        $notification->setHasRead(true);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+
+        // You can redirect to a page that displays submission details
+        return $this->redirectToRoute('student_submissions');
+    }
+
 }
